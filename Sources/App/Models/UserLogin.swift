4016787990d889d011f3f1b6f84900afbac5b984
Darya Varabei <daria14072002@gmail.com>
2022-03-10 22:57:43 +0300

    add: jet configuration

--- Sources/App/Models/UserLogin.swift
+++ Sources/App/Models/UserLogin.swift
@@ -7,3 +7,21 @@
 
 import Foundation
 import Fluent
+
+final class UserLogin: Model {
+    typealias IDValue = Int
+    
+    static let schema = "customers"
+    
+    @ID(key: .id) var user_id: UUID?
+    @Field(key: "email") var email: String
+    @Field(key: "password") var password: String
+
+    init() { }
+
+    init(id: UUID? = nil, email: String, password: String) {
+        self.user_id = id
+        self.email = email
+        self.password = password
+    }
+}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,22 +1,43 @@
 import Fluent
 import FluentPostgresDriver
 import Vapor
+import JWT
+
+extension String {
+    var bytes: [UInt8] { .init(self.utf8) }
+}
+
+extension JWKIdentifier {
+    static let `public` = JWKIdentifier(string: "public")
+    static let `private` = JWKIdentifier(string: "private")
+}
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+//    app.databases.use(.postgres(
+//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+//        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+//        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+//        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+//    ), as: .psql)
 
+    app.databases.use(.postgres(hostname: "localhost", username: "dariavarabei", password: "admin123"), as: .psql)
+    
+    app.migrations.add(CreateUserLogin())
+    app.migrations.add(CreateUser())
+    
+    let privateKey = try String(contentsOfFile: app.directory.workingDirectory + "myjwt.key")
+    let privateSigner = try JWTSigner.rs256(key: .private(pem: privateKey.bytes))
+
+    let publicKey = try String(contentsOfFile: app.directory.workingDirectory + "myjwt.key.pub")
+    let publicSigner = try JWTSigner.rs256(key: .public(pem: publicKey.bytes))
    
-
+    app.jwt.signers.use(privateSigner, kid: .private)
+    app.jwt.signers.use(publicSigner, kid: .public, isDefault: true)
     // register routes
     try routes(app)
 }
