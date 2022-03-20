fc326561e8c71f3d466bf92f9ce576868d9a3722
Darya Varabei <daria14072002@gmail.com>
2022-03-20 21:18:06 +0300

    init

--- Sources/App/Models/Offer.swift
+++ Sources/App/Models/Offer.swift
@@ -7,3 +7,28 @@
 
 import Foundation
 import Fluent
+
+final class Offer: Model, Content {
+  
+    static let schema = "Model"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+//    @Parent(key: "project_id")
+//    var project_id: Project
+    
+    @Field(key: "name")
+    var name: String
+    
+    @Field(key: "location")
+    var location: String
+    
+    @Field(key: "description")
+    var description: String
+    
+    @Children(for: \.$id)
+    var technologies: [Technology]
+    
+    init() {}
+}
